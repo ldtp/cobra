666a8a567f1e6eb5d01c6378d6c0356bd42a6010
Nagappan Alagappan <nalagappan@vmware.com>
2011-12-06 15:01:00 -0800

    Windows LDTP daemon source code
    Windows LDTP daemon source code
    APIs mimic the LDTP framework
    https://reviewboard-qa.eng.vmware.com/r/50670/
    
    QA Notes:
    Testing Done:
    Works on Windows XP SP3 (with .NET 3.0 and 3.5 installed, also accessibility path http://www.microsoft.com/download/en/details.aspx?displaylang=en&id=13821)
    Windows 7
    Windows 7 BATS result - http://racetrack-dev.eng.vmware.com/result.php?id=268804
    Windows XP SP3 BATS result - http://racetrack-dev.eng.vmware.com/result.php?id=268803
    Documentation Notes:
    Bug Number:
    Reviewed by: awu
    Approved by: awu
    Mailto:
    
    [git-p4: depot-paths = "//depot/tools/WinLdtpdService/": change = 1748321]

---
+++ WinLdtpdService/LdtpdService.cs
@@ -0,0 +1,118 @@
+﻿/*
+WinLDTP 1.0
+
+@author: Nagappan Alagappan <nalagappan@vmware.com>
+@copyright: Copyright (c) 2011 VMware Inc.,
+@license: LGPL
+
+http://ldtp.freedesktop.org
+
+This file may be distributed and/or modified under the terms of the GNU General
+Public License version 2 as published by the Free Software Foundation. This file
+is distributed without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.
+
+See 'README.txt' in the source distribution for more information.
+
+Headers in this file shall remain intact.
+*/
+using System;
+//using System.Runtime.Remoting;
+//using System.Runtime.Remoting.Channels;
+//using System.Runtime.Remoting.Channels.Http;
+
+// Additional namespace
+using Ldtpd;
+using System.Net;
+using CookComputing.XmlRpc;
+
+namespace WinLdtpdService
+{
+    class LdtpdService
+    {
+        [STAThread]
+        static void Main()
+        {
+            bool debug = false;
+            string ldtpDebugEnv = Environment.GetEnvironmentVariable("LDTP_DEBUG");
+            string listenAllInterface = Environment.GetEnvironmentVariable("LDTP_LISTEN_ALL_INTERFACE");
+            if (ldtpDebugEnv != null && ldtpDebugEnv.Length > 0)
+                debug = true;
+            /*
+            // If planning to use Remoting instead of HTTP
+            // use this commented portion of code
+            // NOTE: To have this at work, you need to add your
+            // app under Firewall
+            IDictionary props = new Hashtable();
+            props["name"] = "LdtpdService";
+            props["port"] = 4118;
+            HttpChannel channel = new HttpChannel(
+               props,
+               null,
+               new XmlRpcServerFormatterSinkProvider()
+            );
+            ChannelServices.RegisterChannel(channel, false);
+
+            RemotingConfiguration.RegisterWellKnownServiceType(
+              typeof(LdtpdMain),
+              "service.rem",
+              WellKnownObjectMode.Singleton);
+            Console.WriteLine("Press <ENTER> to shutdown");
+            Console.ReadLine();
+            /**/
+            ///*
+            XmlRpcListenerService svc = new Core(debug);
+            HttpListener listener = new HttpListener();
+            // Listen on all possible IP address
+            if (listenAllInterface != null && listenAllInterface.Length > 0)
+            {
+                if (debug)
+                    Console.WriteLine("Listening on all interface");
+                listener.Prefixes.Add("http://*:4118/");
+            }
+            else
+            {
+                if (debug)
+                    Console.WriteLine("Listening only on local interface");
+                // For Windows 8, you need to add firewall rules
+                // Refer: README.txt
+                listener.Prefixes.Add("http://localhost:4118/");
+                listener.Prefixes.Add("http://+:4118/");
+            }
+            listener.Start();
+            try
+            {
+                while (true)
+                {
+                    try
+                    {
+                        if (debug)
+                            Console.WriteLine("Waiting for clients");
+                        HttpListenerContext context = listener.GetContext();
+                        // Don't create LDTP instance here, this creates
+                        // new object for every request !
+                        // Moved before creating HttpListener
+                        //XmlRpcListenerService svc = new LdtpdMain();
+                        if (debug)
+                            Console.WriteLine("Processing request");
+                        svc.ProcessRequest(context);
+                        GC.Collect();
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                listener.Stop();
+            }
+            /**/
+        }
+    }
+}
---
+++ WinLdtpdService/README.txt
@@ -0,0 +1,50 @@
+Verified with Windows XP SP3 / Windows 7 SP1
+
+Minimum requirement .NET3.5
+
+On Windows XP SP3 make sure you have installed:
+.NET3.0 and .NET3.5 and KB971513
+http://www.microsoft.com/download/en/details.aspx?displaylang=en&id=13821 (KB971513)
+
+.NET restributable package download info - http://www.pagestart.com/netframeworkdwnldlinks.html
+
+On Windows 7: Default .NET with the system should work fine
+
+Tested with Python 2.5 / 2.6 / 2.7
+
+Compile SetEnvironmentVariable and WinLdtpdService solutions, place the binary where you have all the dll's, README.txt, lgpl.rtf, before running Wix installer commands
+
+LDTP packages are created with WiX installer - http://wix.tramontana.co.hu
+
+To create WinLDTP package (Credit: David Connet <dconnet@vmware.com>):
+If planing to build package, copy WinLdtpdService.exe to the folder where rest of DLL's exist
+"c:\Program Files (x86)\Windows Installer XML v3.5\bin\candle.exe" -pedantic WinLDTP.wxs
+"c:\Program Files (x86)\Windows Installer XML v3.5\bin\light.exe" -pedantic -spdb -sadv -dcl:high -ext WixUIExtension -ext WixUtilExtension -dWixUILicenseRtf=lgpl.rtf -out WinLDTP.msi WinLDTP.wixobj
+
+By default LDTP listens in localhost, to listen in all ports, set environment variable
+ LDTP_LISTEN_ALL_INTERFACE and then run WinLdtpdService.exe as an user with
+ administrator privillage in Windows 7, else you will get exception Access Denied.
+ Other option is: Disable ACL in Control Panel->User Accounts->Change User Account Control settings
+Other option (Still you need to set LDTP_LISTEN_ALL_INTERFACE):
+netsh http add urlacl url=http://*:4118/MyUri user=User
+
+WinLDTP source files are distributed under LGPLv2.1 license
+Following files are re-distributed as-is
+Microsoft DLL's (Interop.UIAutomationClient.dll, UIAComWrapper.dll, WUIATestLibrary.dll) - http://uiautomationverify.codeplex.com/ - MS-PL license
+ScreenCapture.cs, which doesn't have any license info (Only author name, web site info) - http://www.farooqazam.net/screen-capture-class-for-c-sharp/
+XML RPC .NET library (CookComputing.XmlRpcV2.dll) - http://www.xml-rpc.net/ - MIT X11 license
+
+WinLDTP works based on Microsoft accessibility layer.
+ To check whether your application is accessibility enabled,
+ download the binary from http://uiautomationverify.codeplex.com/ and verify the same.
+
+Verified with Windows 8 developer edition
+
+Minimum requirement .NET4.0
+
+netsh http add urlacl url=http://localhost:4118/MyUri user=User
+netsh http add urlacl url=http://+:4118/MyUri user=User
+
+If you run WinLDTP where you have logged in as a domain user
+netsh http add urlacl url=http://localhost:4118/MyUri user=DOMAIN\User
+netsh http add urlacl url=http://+:4118/MyUri user=DOMAIN\User
