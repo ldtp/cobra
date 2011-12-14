04ada9393f91d8eed3af0e19c03289e687553b9c
Nagappan Alagappan <nalagappan@vmware.com>
2011-12-14 13:46:41 -0800

    Added project description
    Added project description
    
    QA Notes:
    Testing Done:
    Documentation Notes:
    Bug Number:
    Reviewed by:
    Approved by:
    Mailto:
    
    [git-p4: depot-paths = "//depot/tools/WinLdtpdService/": change = 1751242]

--- Ldtpd/Properties/AssemblyInfo.cs
+++ Ldtpd/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Ldtpd")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Linux Desktop Testing Project")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("VMware Inc")]
 [assembly: AssemblyProduct("Ldtpd")]
