0499257170fe94dea51dd0d8c762070c20ff8526
Nagappan Alagappan <nagappan@gmail.com>
2012-07-18 17:42:11 -0700

    Removed empty kwargs

--- CobraWinLDTP/ldtp/client.py
+++ CobraWinLDTP/ldtp/client.py
@@ -62,9 +62,6 @@ class _Method(xmlrpclib._Method):
         logger.debug('%s(%s)' % (self.__name, \
                                      ', '.join(map(repr, args) + ['%s=%s' % (k, repr(v)) \
                                                                       for k, v in kwargs.items()])))
-        if not _ldtp_windows_env:
-            # Empty kwargs, doesn't work ! with Windows
-            args += (kwargs,)
         return self.__send(self.__name, args)
 
 class Transport(xmlrpclib.Transport):
