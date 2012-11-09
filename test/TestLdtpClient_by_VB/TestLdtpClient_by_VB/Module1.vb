7b86ae0ad37cfeca6240c67698b9810872fa29c4
Nagappan Alagappan <nagappan@gmail.com>
2012-11-09 14:50:19 -0800

    Added VB test client

---
+++ test/TestLdtpClient_by_VB/TestLdtpClient_by_VB/Module1.vb
@@ -0,0 +1,9 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim ldtp As New Ldtp.Ldtp("*Notepad")
+        Console.WriteLine("Notepad: " + ldtp.GuiExist().ToString())
+        Console.WriteLine("Word Wrap: " + ldtp.VerifyMenuCheck("mnuFormat;mnuWordWrap").ToString())
+    End Sub
+
+End Module
