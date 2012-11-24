a09559aeb0e0cc53403c1ca7bc90e376ec97dcc9
Nagappan Alagappan <nagappan@gmail.com>
2012-11-23 19:45:19 -0800

    Moved examples to respective language folder

--- Example/TestLdtpClient_by_VB/TestLdtpClient_by_VB/Module1.vb
+++
@@ -1,9 +0,0 @@
-﻿Module Module1
-
-    Sub Main()
-        Dim ldtp As New Ldtp.Ldtp("*Notepad")
-        Console.WriteLine("Notepad: " + ldtp.GuiExist().ToString())
-        Console.WriteLine("Word Wrap: " + ldtp.VerifyMenuCheck("mnuFormat;mnuWordWrap").ToString())
-    End Sub
-
-End Module
