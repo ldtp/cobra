69b8b47777401b3db796006a0cba2cfcb035f086
Nagappan Alagappan <nagappan@gmail.com>
2013-09-08 10:32:38 -0700

    Work from https://github.com/majorsilence/ldtp2

---
+++ Example/C#/TestLdtpClientNotepadPlus/TestLdtpClient/Example1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Ldtp;
+
+namespace TestLdtpClient
+{
+    class Example1
+    {
+
+        public static void Test()
+        {
+
+            Ldtp.Ldtp l = new Ldtp.Ldtp("*Notepad*");
+            l.WaitTillGuiExist();
+            l.SelectMenuItem("mnuFile;mnuNew");
+
+        }
+
+    }
+}
