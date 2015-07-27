4f77806f736406d8502d97162bb8702127efcebd
Zitrax <daniel@bengtssons.info>
2015-07-27 11:35:52 +0200

    Added build_installer.bat
    The script will build the solution using msbuild and then create
    the msi installer using wix.
    
    I have updated the Source locations in the .wxs file to avoid
    having to copy files around which was confusing and easy to mess up.
    
    Currently hardcoded to use the Release versions of the files and
    using my tool locations by default.
    
    But note: Does not handle/build the .jar file atm.

---
+++ build_installer.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+REM Configure tool locations
+SET MSBUILD=C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe
+SET WIX=C:\Program Files (x86)\WiX Toolset v3.9\bin
+
+REM ===========================================================
+
+"%MSBUILD%" -t:Build -p:Configuration=Release || GOTO END
+
+REM FIXME: JUST SINCE I DON'T NEED IT ATM
+COPY NUL CobraWinLDTP\ldtp\Ldtp.jar || GOTO END
+
+"%WIX%\candle.exe" -pedantic CobraWinLDTP\CobraWinLDTP.wxs -out CobraWinLDTP\CobraWinLDTP.wixobj || GOTO END
+"%WIX%\light.exe" -pedantic -spdb -dcl:high -ext WixUIExtension -ext WixUtilExtension -dWixUILicenseRtf=License.rtf -out CobraWinLDTP.msi CobraWinLDTP\CobraWinLDTP.wixobj || GOTO END
+EXIT /B 0
+
+:END
+echo "Command errored, aborting..."
+EXIT /B %ERRORLEVEL%
